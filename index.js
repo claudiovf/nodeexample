c85c8f879bb82b9710e89abcd50ab9ac10041bde
Claudio <claudiovf@hotmail.com>
2020-08-06 16:59:12 +1000

    First commit

---
+++ index.js
@@ -0,0 +1,94 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+
+//json parsing
+app.use(express.json())
+
+let notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      date: "2019-05-30T17:30:31.098Z",
+      important: true
+    },
+    {
+      id: 2,
+      content: "Browser can execute only Javascript",
+      date: "2019-05-30T18:39:34.091Z",
+      important: false
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      date: "2019-05-30T19:20:14.298Z",
+      important: true
+    }
+]
+
+app.get('/',(req, res) => {
+    res.send('<h1>Hello World</h1>')
+})
+
+app.get('/api/notes', (req,res) => {
+    res.json(notes)
+})
+
+app.get('/api/notes/:id', (req,res) => {
+    const id = Number(req.params.id)
+    const note = notes.find(note => note.id === id)
+
+    if (note) {
+        res.json(note)
+    } else {
+        res.status(404).end()
+    }
+})
+
+//..notes.map will return values to Math instead of an array
+//like notes.map would
+const generatedId = () => {
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => n.id)) 
+        : 0
+
+    return maxId +1
+}
+app.post('/api/notes', (req, res) => {
+    const body = req.body
+
+    
+    if(!body.content) {
+        // must return otherwise it will run to the end
+        return res.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generatedId(),
+    }
+
+    notes = notes.concat(note)
+    res.json(note)
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+    const id = Number(req.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    console.log(id, notes)
+
+    res.status(204).end()
+})
+
+
+const PORT = process.env.PORT || 3002
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
