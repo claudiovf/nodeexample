00779a0ecb8d6bad4e5dcb7bc0329c2f83fd2f24
Claudio <claudiovf@hotmail.com>
2020-08-13 14:27:37 +1000

    uibuild

--- index.js
+++ index.js
@@ -14,92 +14,92 @@ app.use(express.static('build'))
 
 
 app.get('/api/notes', (req,res) => {
-    Note
-        .find({})
-        .then(notes => {
-            res.json(notes)
-        })
+  Note
+    .find({})
+    .then(notes => {
+      res.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (req,res, next) => {
-    Note.findById(req.params.id)
-        .then(note => {
-            if(note) {
-                res.json(note)
-            }else {
-                res.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+  Note.findById(req.params.id)
+    .then(note => {
+      if(note) {
+        res.json(note)
+      }else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 //..notes.map will return values to Math instead of an array
 //like notes.map would
 
 app.post('/api/notes', (req, res, next) => {
-    const body = req.body
-    
-    if(!body.content) {
-        // must return otherwise it will run to the end
-        return res.status(400).json({
-            error: 'content missing'
-        })
-    }
+  const body = req.body
 
-    const note = new Note({
-        content: body.content,
-        important: body.important || false,
-        date: new Date(),
+  if(!body.content) {
+    // must return otherwise it will run to the end
+    return res.status(400).json({
+      error: 'content missing'
     })
-    note
-        .save()
-        .then(savedNote => savedNote.toJSON())
-        .then(savedAndFormatted => {
-            res.json(savedAndFormatted)
-        })
-        .catch(error => next(error))
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  })
+  note
+    .save()
+    .then(savedNote => savedNote.toJSON())
+    .then(savedAndFormatted => {
+      res.json(savedAndFormatted)
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (req, res, next) => {
-    Notes.findByIdAndRemove(req.params.id)
-        .then(result => {
-            res.status(204).end()
-        })
-        .catch(error => next(error))
+  Note.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 
 })
 
 app.put('/api/notes/:id', (req, res, next) => {
-    const body = req.body
-    
-    const note = {
-        content: body.content,
-        important: body.important,
-    }
+  const body = req.body
 
-    Note.findByIdAndUpdate(req.body.id, note, {new: true})
-        .then(updatedNote => {
-            res.json(updatedNote)
-        })
-        .catch(error => next(error))
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(req.body.id, note, { new: true })
+    .then(updatedNote => {
+      res.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-  }
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.use(unknownEndpoint)
 
 
 const errorHandler = (error, req, res, next) => {
-    console.log(error.message)
-    if(error.name === 'CastError') {
-        return res.status(400).send({ error: 'malformatted id'})
-    } else if (error.name === 'ValidationError') {
-        return res.status(400).json({ error: error.message })
-    }
+  console.log(error.message)
+  if(error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 app.use(errorHandler)
 
@@ -107,5 +107,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
